6b8bf081c219dfa96cff97013ad334eb67e3e819
administrator <cpacash.business@gmail.com>
2023-09-21 00:09:24 +0300

    fix balance report

--- source/handlers/users/report_operations/get_balance_in_report.py
+++ source/handlers/users/report_operations/get_balance_in_report.py
@@ -2,7 +2,7 @@ from aiogram import Router, F
 from aiogram.fsm.context import FSMContext
 from aiogram.types import Message
 from components.filters import IsUserFilter
-from components.texts.users.get_balance_in_report import text_user_balances
+from components.texts.users.get_balance_in_report import text_user_balances, text_no_reports
 from services.google_api.google_table import GoogleTable
 from services.redis_models.user import RedisUser
 from services.sql_models_extends.user import UserExtend
@@ -26,14 +26,18 @@ async def get_balance_in_report(message: Message, state: FSMContext, gt_object: 
         chat_id_user=message.from_user.id
     )
 
-    balances.sort(key=lambda e: e[0])
+    if balances:
+        balances.sort(key=lambda e: e[0])
 
-    last_org = ""
-    for b in balances:
-        if b[0] != last_org:
-            text_balances += f"\n<b>{b[0]}</b> ⤵️\n{b[1]}:  <code>{b[2]}</code> руб.\n"
-            last_org = b[0]
-        else:
-            text_balances += f"{b[1]}:  <code>{b[2]}</code> руб.\n"
+        last_org = ""
+        for b in balances:
+            if b[0] != last_org:
+                text_balances += f"\n<b>{b[0]}</b> ⤵️\n{b[1]}:  <code>{b[2]}</code> руб.\n"
+                last_org = b[0]
+            else:
+                text_balances += f"{b[1]}:  <code>{b[2]}</code> руб.\n"
 
-    await message.answer(text=text_balances, parse_mode="html")
+        await message.answer(text=text_balances, parse_mode="html")
+
+    else:
+        await message.answer(text=text_no_reports, parse_mode="html")
