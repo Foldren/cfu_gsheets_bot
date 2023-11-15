d07aaa933fcd6a6ea8d27d1f73325e9e215d8eb4
administrator <cpacash.business@gmail.com>
2023-11-15 14:40:36 +0300

    fix all

--- source/microservices/sql_models_extends/user.py
+++ source/microservices/sql_models_extends/user.py
@@ -205,11 +205,13 @@ class UserExtend:
                                  admin_id=id_admin, bet=bet, increased_bet=increased_bet)
         await Role.bulk_create([Role(user_id=chat_id, type='normal'), Role(user_id=chat_id, type='report_request')])
 
+        print(google_table_url.encode('utf-8'))
+
         if id_admin is None:
             await AdminInfo.create(
                 admin_id=chat_id,
-                google_table_url=Fernet(SECRET_KEY).encrypt(google_table_url.encode()),
-                google_drive_dir_url=Fernet(SECRET_KEY).encrypt(google_drive_dir_url.encode())
+                google_table_url=Fernet(SECRET_KEY).encrypt(google_table_url.encode('utf-8')),
+                google_drive_dir_url=Fernet(SECRET_KEY).encrypt(google_drive_dir_url.encode('utf-8'))
             )
             await user.periods_stats.add(*(await PeriodStat.all()))
 
